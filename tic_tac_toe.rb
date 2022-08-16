18c6878fbade436a83e870feda8a5aaffd7f0f9a
khoa <khoa.soft.e@gmail.com>
2022-08-16 21:40:37 +0700

    completed

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,6 +4,8 @@ WINNING_COMPS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8
 
 # Player
 class Player
+  attr_reader :name
+
   def initialize(name, sign)
     @position = []
     @name = name
@@ -11,19 +13,31 @@ class Player
   end
 
   def play(board)
-    puts 'Choose a position (1 to 9) to place your sign'
-    index = gets.chomp.to_i
-    @position.push[index - 1]
-    board.update(index - 1, @sign)
+    puts "#{@name}'s turn, please choose a position (1 to 9) to place your sign"
+    # get valid input from user
+    index = nil
+    loop do
+      index = gets.chomp.to_i - 1
+      break if index.between?(0, 8) && board.current_state[index].is_a?(Integer)
+
+      puts 'Invalid input, please try again'
+    end
+    @position.push(index)
+    board.update(index, @sign)
   end
 
   def win?
-    WINNING_COMPS.any? { |comp| comp == @position }
+    WINNING_COMPS.each do |combo|
+      return true if combo.all? { |i| @position.include?(i) }
+    end
+    false
   end
 end
 
 # Representing the play board with an array, each element is a title on the board
 class Board
+  attr_reader :current_state
+
   def initialize
     @current_state = Array.new(9)
     @current_state = @current_state.each_index.map { |index| index + 1 }
@@ -53,13 +67,25 @@ def play_round(player1, player2, board, turn)
   turn.odd? ? player1.play(board) : player2.play(board)
 end
 
+def result_annoucement(player1, player2)
+  if player1.win?
+    puts "#{player1.name} wins!"
+  elsif player2.win?
+    puts "#{player2.name} wins!"
+  else
+    puts 'Draw!'
+  end
+end
+
 def play_game(player1, player2, board)
   turn = 1
-  while !board.game_ended? && !(player1.win? || player2.win?)
+  until board.game_ended? || player1.win? || player2.win?
     board.display
     play_round(player1, player2, board, turn)
     turn += 1
   end
+  board.display
+  result_annoucement(player1, player2)
 end
 
 board = Board.new
